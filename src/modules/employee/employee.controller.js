b1fc927030cf888988d8c6420749c7179ca52661
Guilherme <03.09gui.mafaldo@gmail.com>
2025-04-01 08:34:54 -0300

    feat: finish at configs

--- src/modules/employee/employee.controller.js
+++ src/modules/employee/employee.controller.js
@@ -4,7 +4,7 @@ export const createEmployee = async (req, res) => {
     const { name, email, role, salary, admissionDate, phone, password } = req.body
 
     if(!name || !email || !role || !salary || !admissionDate || !phone || !password) {
-        res.status(400).json({ Error: 'Tods os campos são obrigatórios' })
+        res.status(400).json({ Error: 'Todos os campos são obrigatórios' })
         return
     }
     try {
@@ -23,8 +23,8 @@ export const createEmployee = async (req, res) => {
             newEmployee
         })
     } catch(Execption) {
-        console.error('Erro ao adicionar Funcionario', Execption)
-        res.status(500).json({ Error: 'Erro internio do servidor.' })
+        console.error('Erro ao adicionar Funcionario', Execption.message)
+        res.status(500).json({ Error: 'Erro interno do servidor.' })
     }
 }
 export const getEmployee = async (req, res) => {
@@ -35,7 +35,66 @@ export const getEmployee = async (req, res) => {
             employees
          })
     } catch(Exception){
-        console.error('Erro ao buscar Funcionario', Exception)
-        return res.status(500).json({ Error: 'Erro internio do servidor.' })
+        console.error('Erro ao buscar Funcionario', Exception.message)
+        return res.status(500).json({ Error: 'Erro interno do servidor.' })
+    }
+}
+
+export const updateEmployee = async (req, res) => {
+    const { id } =  req.params
+    const { name, email, role, salary, admissionDate, phone, password } = req.body
+
+    try {
+        const getEmployeeById = await employeeService.getEmployeeById(id)
+
+        if(!getEmployeeById){
+            return res.status(404).json({ 
+                Message: 'Nenhum colaborador encontrado com o ID informado.',
+                UserId: id
+            })
+        }
+
+        const newInfosEmployee = {}
+        if(name) newInfosEmployee.name = name
+        if(email) newInfosEmployee.email = email
+        if(role) newInfosEmployee.role = role
+        if(salary) newInfosEmployee.salary = salary
+        if(admissionDate) newInfosEmployee.admissionDate = admissionDate
+        if(phone) newInfosEmployee.phone = phone
+        if(password) newInfosEmployee.password = password
+
+        if (Object.keys(newInfosEmployee).length === 0) {
+            return res.status(400).json({ Error: 'Nenhum campo valido foi alterado.' })
+        }
+
+        const infosEmployee = await employeeService.updateEmployee(id, newInfosEmployee)
+
+        return res.status(200).json({ 
+            Message: 'Dados alterados com sucesso.',
+            UpdatedEmployee: infosEmployee
+        })
+    } catch(Exception) {
+        console.error('Erro ao atualizar Dados do colaborador:', Exception.message)
+        return res.status(500).json({ Error: 'Erro interno do servidor.' })
+    }
+}
+
+export const deleteEmployee = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const deleteEmployee = await employeeService.getEmployeeById(id)
+
+        if(!deleteEmployee) {
+            return res.status(404).json({ Message: 'Nenhum colaborador encontrado com o ID informado.' })
+        }
+
+        return res.status(200).json({ 
+            Message: 'Colaborador excluido da base de dados com sucesso.'
+         })
+         
+    } catch(Exception) {
+        console.error('Erro ao Deletar colaborador da base de dados:', Exception.message)
+        return res.status(500).json({  Error: 'Erro interno do servidor.' })
     }
 }
--- src/modules/employee/employee.service.js
+++ src/modules/employee/employee.service.js
@@ -1,22 +1,55 @@
-import prisma from '../../../prisma/prismaClient.jsgi '
+import prisma from '../../../prisma/prismaClient.js '
 
 export const createNewEmployee = async(data) => {
+    const { name, email, role, salary, admissionDate, phone, password } = data
+
+    
     if(!data.email.includes('@')) {
         throw new Error('Insira um E-mail válido')
     }
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-    const newEmployee =await prisma.employee.create({
+    const newEmployee = await prisma.employee.create({
         data: {
-            name: data.name,
-            email: data.email,
-            role: data.role,
-            salary: data.salary,
-            admissionDate: data.admissionDate,
-            phone: data.phone,
-            password: data.password
+            name,
+            email,
+            role,
+            salary,
+            admissionDate,
+            phone,
+            password: hashedPassword
         }
     })
     return newEmployee
+}
+
+export const updateEmployee = async (id, data) => {
+    const employee = await prisma.employee.findUnique({ where: { id}})
+    if(!employee) {
+        return null
+    }
+
+    const updateData = { ...data}
+
+    if (data.password) {
+        updateData.password = await bcrypt.hash(data.password, 10)
+    }
+
+    return prisma.employee.update({
+        where: { id },
+        data: updateData
+    })
+}
+
+export const deleteEmployee = async (id) => {
+    const employee = await prisma.employee.findUnique({ where: { id }})
+
+    if(!employee) {
+        return null
+    }
+
+    await prisma.employee.delete({ where: { id }})
+    return  { Message: 'Colaborador excluido da base de dados.' }
 }
 
 export const listAllEmployees = async () => {
